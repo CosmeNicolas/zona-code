d3056dc68d43fa2d766b4a914de0426c7c67525d
CosmeNicolas <nicomas7590@gmail.com>
2023-12-11 23:26:36 -0300

    agregue las funciones de mostrar modal de oferta al cargar la pagina principal

---
+++ js/app.js
@@ -0,0 +1,11 @@
+/* MostarModal al cargar la pagina */
+const mostrarModalOferta = new bootstrap.Modal(
+  document.getElementById('mostrarOferta')
+)
+
+
+const mostrarModal = ()=>{
+  mostrarModalOferta.show()
+}
+
+document.addEventListener('DOMContentLoaded', mostrarModal);
