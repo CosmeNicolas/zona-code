e67cfaf59216bc7a4adb5cca575796bb8d95d3b9
Adrian Chanampa <adrian.karma.chan@gmail.com>
2023-12-13 22:34:48 -0300

    modificacion en constantes y asignaciones

--- js/app-administracion.js
+++ js/app-administracion.js
@@ -1,13 +1,13 @@
 const modalAdminProducto = new bootstrap.Modal(
   document.getElementById("administrarProducto")
 );
-const btnAgregarProducto = document.getElementById("btnNuevoContacto");
+const btnAgregarProducto = document.getElementById("btnNuevoProducto");
 const formularioProducto = document.querySelector("form");
-const nombre = document.getElementById("nombre"),
+const id = document.getElementById("id"),
+  nombre = document.getElementById("nombre"),
   precio = document.getElementById("precio"),
   categoria = document.getElementById("categoria"),
   imagen = document.getElementById("imagen"),
   descripcion = document.getElementById("descripcion"),
-  stock = document.getElementById("stock"); 
-
-const agenda = JSON.parse(localStorage.getItem("agendaKey")) || [];
+  stock = document.getElementById("stock");
+const inventario = JSON.parse(localStorage.getItem("inventarioKey")) || [];
