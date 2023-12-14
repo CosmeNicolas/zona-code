c3c29b3480f241fdfbb39a20e3a822cc707f4245
Antonella Ortega <antonellaportega@gmail.com>
2023-12-14 00:01:55 -0300

    agregue las imaganes del producto

---
+++ js/Detalle.js
@@ -0,0 +1,13 @@
+let images = Array.from(document.getElementsByClassName("imgCarousel"))
+
+let mainPhoto = document.getElementById("mainPhoto")
+
+function updateImage(event){
+    let image = event.target
+
+    mainPhoto.src = image.src
+}
+
+images.forEach(function(image){
+    image.addEventListener("click",updateImage)
+});
