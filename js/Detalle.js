0d0f039ce161eda7bf7e5c9611eec9f33648f985
Antonella Ortega <antonellaportega@gmail.com>
2023-12-14 00:09:32 -0300

    agregue funcionalidad al bboton de cantidad

--- js/Detalle.js
+++ js/Detalle.js
@@ -1,3 +1,4 @@
+//IMAGENES DE LOS PRODUCTOS
 let images = Array.from(document.getElementsByClassName("imgCarousel"))
 
 let mainPhoto = document.getElementById("mainPhoto")
@@ -11,3 +12,20 @@ function updateImage(event){
 images.forEach(function(image){
     image.addEventListener("click",updateImage)
 });
+
+
+// BOTON DE CANTIDAD
+
+function incrementarCantidad() {
+    let inputCantidad = document.getElementById("inputCantidad");
+    let cantidad = parseInt(inputCantidad.value, 10) || 0;
+    inputCantidad.value = cantidad + 1;
+  }
+
+  function decrementarCantidad() {
+    let inputCantidad = document.getElementById("inputCantidad");
+    let cantidad = parseInt(inputCantidad.value, 10) || 0;
+    if (cantidad > 1) {
+      inputCantidad.value = cantidad - 1;
+    }
+  }
