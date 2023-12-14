b2f1c2b1168a7732b6b16dbea354f984b7af1a6a
Adrian Chanampa <adrian.karma.chan@gmail.com>
2023-12-14 12:16:11 -0300

    creo app-classProducto con el constructor de producto

---
+++ js/app-classProducto.js
@@ -0,0 +1,22 @@
+export default class Contacto {
+    #id;
+    #nombre;
+    #precio;
+    #categoria;
+    #imagen;
+    #descripcion;
+    #stock;
+    
+  
+    constructor(id = crypto.randomUUID() , nombre, precio, categoria, imagen, descripcion, stock) {
+      this.#id = id;
+      this.#nombre = nombre;
+      this.#precio = precio;
+      this.#categoria = categoria;
+      this.#imagen = imagen;
+      this.#descripcion = descripcion;
+      this.#stock = stock;
+     
+    }    
+  }
+  
