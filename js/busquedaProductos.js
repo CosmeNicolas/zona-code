dd886aada1bb7227fcc7e1b9e15f534890f798a7
CosmeNicolas <nicomas7590@gmail.com>
2023-12-16 20:19:21 -0300

    agregue la funcion de boton buscar en el navbar

--- js/busquedaProductos.js
+++ js/busquedaProductos.js
@@ -1,25 +1,42 @@
-let formularioBusqueda = document.getElementById('formularioDeBusqueda')
-let resultadoRenglonBusqueda = document.getElementById('renglonBusqueda')
+let formularioBusqueda = document.getElementById('formularioDeBusqueda');
+let resultadoRenglonBusqueda = document.getElementById('renglonBusqueda');
+let mensaje = document.getElementById("sincoincidencias");
+let search = document.getElementById("renglonBusqueda");
 
+formularioBusqueda.addEventListener('submit', (e) => {
+  e.preventDefault();
+});
 
-formularioBusqueda.addEventListener('submit', (e)=>{
-  e.preventDefault()
-})
+const productosTotales = document.querySelectorAll(".card");
 
-//ver queryselector articulo 
-const productosTotales = document.querySelectorAll(".producto-card")
+document.addEventListener('keyup', (e) => {
+  if (e.key === "Escape") {
+    e.target.value = "";
+  } 
+  if (e.target.matches('#renglonBusqueda')) {
+    const searchTerm = e.target.value.toLowerCase();
 
-console.log(productosTotales)
+    const productosFiltrados = Array.from(productosTotales).filter(producto => {
+      const textoProducto = producto.children[1].children[0].innerHTML.toLowerCase();
+      return textoProducto.includes(searchTerm);
+    });
 
+    productosTotales.forEach(producto => {
+      const coincide = productosFiltrados.includes(producto);
+      if (coincide) {
+        producto.style.display = "col-12";
+        producto.classList.remove('ocultarProducto');
+      } else {
+        producto.style.display = 'none';
+        producto.classList.add('ocultarProducto');
+      }
+    });
 
-//evento "keyup" - indica q letra fue presionada al soltar
-document.addEventListener('keydown', (e)=>{
-  if(e.key === "Escape") e.target.value = "";
-  if(e.target.matches('#renglonBusqueda')){
-    console.log('se uso una tecla')
-    productosTotales.forEach((producto)=>{
-      producto.children[1].children[0].includes(e.target.value)
-    })
+     const productosOcultos = document.querySelectorAll('.ocultarProducto');
+    if (productosOcultos.length >= productosTotales.length) {
+      mensaje.className = "fs-1 text-center text-light my-5";
+    } else {
+      mensaje.className = "fs-2 d-none";
+    }
   }
-  
-})
+});
