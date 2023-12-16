4360a02650d69235bbbce4889bf3168dbd100283
Julián Toranzo Navarro <julianalmacenamiento20@gmail.com>
2023-12-15 23:47:34 -0300

    hecha la parte de Create del CRUD

--- js/app-administrarUsuario.js
+++ js/app-administrarUsuario.js
@@ -1,1 +1,58 @@
 import Usuario from "./classUsuario.js";
+
+const modalRegistrarUsuario = new bootstrap.Modal(document.getElementById ('registrarUsuario'));
+const btnAgregarUsuario = document.getElementById('btnAgregarUsuario');
+const formularioUsuario = document.getElementById('formularioUsuario')
+const nombre = document.getElementById('nombreUsuario'),
+apellido = document.getElementById('apellido'),
+email = document.getElementById('emailUsuario'),
+contraseña = document.getElementById('contraseña');
+
+const mostrarModal = () =>{
+    modalRegistrarUsuario.show();
+}
+
+
+const crearUsuario = (e)=>{
+    e.preventDefault();
+    console.log('aqui debo crear el usuario nuevo')
+
+    const nuevoUsuario = new Usuario(undefined, nombre.value, apellido.value, email.value, contraseña.value );
+    console.log(nuevoUsuario)
+}
+
+
+
+
+btnAgregarUsuario.addEventListener('click', mostrarModal);
+formularioUsuario.addEventListener('submit', crearUsuario)
+
+
+
+
+/*
+
+const modalAdminUsuario = new bootstrap.Modal(document.getElementById('registrarUsuario'));
+const btnRegistrarUsuario = document.getElementById('btnNuevoUsuario');
+const formularioUsuario = document.querySelector('form');
+const nombre = document.getElementById('nombre'),
+apellido = document.getElementById('apellido'),
+email = document.getElementById('email'),
+contraseña = document.getElementById('contraseña');
+
+const mostrarModal = ()=>{
+    modalRegistrarUsuario.show();
+}
+
+const crearUsuario = (e) =>{
+    e.preventDefault();
+console.log('aqui debo ver el nuevo usuario')
+
+const nuevoUsuario = new Usuario(undefined, nombre.value, apellido.value, email.value, contraseña.value );
+console.log(nuevoUsuario)
+}
+
+btnRegistrarUsuario.addEventListener('click', mostrarModal);
+formularioUsuario.addEventListener('submit', crearUsuario)
+
+*/
--- js/classUsuario.js
+++ js/classUsuario.js
@@ -1,53 +1,52 @@
 export default class Usuario{
-    #id;
-    #nombre;
-    #apellido;
-    #email;
+  #id;
+  #nombre;
+  #apellido;
+  #email;
+  #contraseña;
 
-    constructor(ID= crypto.randomUUID(),nombre, apellido,email,celular){
-        this.#id = id;
-        this.#nombre = nombre;
-        this.#apellido = apellido;
-        this.#email = email;
-    }
+  constructor(id = crypto.randomUUID() , nombre, apellido, email, contraseña) {
+    this.#id = id;
+    this.#nombre = nombre;
+    this.#apellido = apellido;
+    this.#email = email;
+    this.#contraseña = contraseña;
+  }
 
-    get id(){
-        return this.#id;
-    }
+  get id() {
+    return this.#id;
+  }
 
-    get nombre(){
-        return this.#nombre;
-    }
+  get nombre() {
+    return this.#nombre;
+  }
 
-    set nombre(nuevoNombre) {
-        this .#nombre = nuevoNombre;
-    }
+  set nombre(nuevoNombre) {
+    this.#nombre = nuevoNombre;
+  }
 
-    get apellido(){
-        return this.#apellido;
-    }
+  get apellido() {
+    return this.#apellido;
+  }
 
-    set apellido(nuevoApellido) {
-        this .#apellido = nuevoApellido;
-    }
+  set apellido(nuevoApellido) {
+    this.#apellido = nuevoApellido;
+  }
 
-    get email(){
-        return this.#email;
-    }
+  get email() {
+    return this.#email;
+  }
 
-    set email(nuevoEmail) {
-        this .#email = nuevoEmail;
-    }
-}
-/*
-// este método sirve para el objeto Json.stringify
-toJSON(){
-    return{
-        id: this.id,
-        apellido: this.apellido,
-        nombre: this.nombre,
-        email: this.email,
-    }
-}
+  set email(nuevoEmail) {
+    this.#email = nuevoEmail;
+  }
 
-}*/
+  get contraseña() {
+     return this.#contraseña;
+  }
+  
+  set contraseña(nuevaContraseña) {
+    this.#contraseña = nuevaContraseñaontraseña;
+    }
+  }
+
