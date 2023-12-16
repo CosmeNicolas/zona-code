fc81aeffccc82e07d02739bc0e32ced04faea9c2
Antonella Ortega <antonellaportega@gmail.com>
2023-12-16 15:53:06 -0300

    hover a las card de los productos

--- js/ProductoVista.js
+++ js/ProductoVista.js
@@ -8,10 +8,9 @@ document.addEventListener('DOMContentLoaded', () => {
         card.className = 'card m-2 col-lg-3 col-md-5 col-12 '; // Clases de Bootstrap aplicadas aquí
         card.innerHTML = `
             <img src="${producto.imagen}" alt="${producto.nombre}" class="card-img-top pt-2>
-            <div class="card-body">
+            <div class="card-body ">
                 <h5 class="card-title text-light py-2">${producto.nombre}</h5>
-                <p class="card-text text-light">${producto.descripcion}</p>
-               
+                <p class="card-text text-light">${producto.descripcion}</p>  
             </div>
             <div class="card-footer text-light">
             <div class="d-flex justify-content-around">
