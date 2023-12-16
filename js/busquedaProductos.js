0c83b328985814c5cee48961735b9363e4ec2bf8
CosmeNicolas <nicomas7590@gmail.com>
2023-12-16 18:45:20 -0300

    agrego al archivo prodcutovista.js en la card la clase producto-card

--- js/busquedaProductos.js
+++ js/busquedaProductos.js
@@ -1,1 +1,18 @@
-let formularioBusque = document.getElementById('formularioDeBusqueda')
+let formularioBusqueda = document.getElementById('formularioDeBusqueda')
+let resultadoRenglonBusqueda = document.getAnimations.getElementById('renglonBusqueda')
+
+
+formularioBusqueda.addEventListener('submit', (e)=>{
+  e.preventDefault()
+})
+
+//ver queryselector articulo 
+
+//evento "keyup" - indica q letra fue presionada al soltar
+document.addEventListener('keyup', (e)=>{
+  if(e.key === "Escaoe") e.target.value = "";
+  if(e.target.matches('#renglonBusqueda')){
+    console.log('se uso una tecla')
+  }
+  
+})
