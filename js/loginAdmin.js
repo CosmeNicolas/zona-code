faf205ab4a27d92049f805e092f099f3a0af9ba5
Pablo Moyano <pabmoy1725@gmail.com>
2023-12-17 17:19:48 -0300

    se realiza maquetado de loginAdmin

---
+++ js/loginAdmin.js
@@ -0,0 +1,10 @@
+const modalAmin = new bootstrap.Modal(
+    document.getElementById("contactoAdmin")
+  );
+  const abrirModalAdmin = document.getElementById("abrirModalAdmin");
+  
+  const mostrarModalAdmin = () => {
+    modalAmin.show();
+  };
+  
+  abrirModalAdmin.addEventListener("click", mostrarModalAdmin);
