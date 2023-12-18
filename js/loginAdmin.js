d0c03e6066e46cb0a0806ac2324db4bc4716a73d
Pablo Moyano <pabmoy1725@gmail.com>
2023-12-18 17:18:45 -0300

    Se aplicaron expresiones regulares y validaciones al usuario Admin

--- js/loginAdmin.js
+++ js/loginAdmin.js
@@ -1,43 +1,62 @@
-const modalAmin = new bootstrap.Modal(document.getElementById("contactoAdmin"));
+const modalAdmin = new bootstrap.Modal(document.getElementById("contactoAdmin"));
 const abrirModalAdmin = document.getElementById("abrirModalAdmin");
-
-
-
-const mostrarModalAdmin = () => {
-  modalAmin.show();
-  
-};
+const formLogin = document.getElementById("formLogin");
+const errorMensaje = document.getElementById("errorMensaje");
 
 abrirModalAdmin.addEventListener("click", mostrarModalAdmin);
 
+function mostrarModalAdmin() {
+  modalAdmin.show();
+}
 
 function iniciarSesion() {
   let usuario = document.getElementById("usuario").value;
   let contraseña = document.getElementById("clave").value;
   let errorMensaje = document.getElementById("errorMensaje");
 
-
-  if (usuario.trim() === "" || contraseña.trim() === "") {
-    errorMensaje.textContent = "Por favor, ingrese usuario y contraseña.";
+  if (!validarCampos(usuario, contraseña)) {
     return;
   }
 
   if (usuario === "Gamer" && contraseña === "1234") {
     window.location = "administracion.html";
-  } else errorMensaje.textContent = "Usuario o Contraseña incorrectos";
+  } else {
+    errorMensaje.textContent = "Usuario o Contraseña incorrectos";
+    limpiarFormulario();
+  }
 
-  limpiarFormulario();
+
+}
+ 
+function validarCampos(usuario, contraseña) {
+  const soloLetrasNumeros = /^[a-zA-Z0-9]+$/;
+  const soloNumeros = /^\d+$/;
+
+  if (usuario.trim() === "" || !soloLetrasNumeros.test(usuario)) {
+    errorMensaje.textContent = "El usuario debe contener solo letras y números.";
+    return false;
+    
+  }
+
+  if (contraseña.trim() === "" || !soloNumeros.test(contraseña)) {
+    errorMensaje.textContent = "La contraseña debe contener solo números.";
+    return false;
+  }
+
+  return true;
+  
 }
 
 function limpiarFormulario() {
-    formLogin.reset();
-    
+  formLogin.reset();
+  
 }
 
 function cerrar() {
-    document.getElementById("usuario").value = "";
-    document.getElementById("clave").value = "";
-    document.getElementById("errorMensaje").textContent = "";
+  document.getElementById("usuario").value = "";
+  document.getElementById("clave").value = "";
+  document.getElementById("errorMensaje").textContent = "";
 }
+
 
 
