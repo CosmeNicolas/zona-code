35510d2365b24ad726bbb1ebc0d7f53936248219
Pablo Moyano <pabmoy1725@gmail.com>
2023-12-18 16:37:56 -0300

    Se agrego logica para borrar datos de loginAdmin al cerrar el modal

--- js/loginAdmin.js
+++ js/loginAdmin.js
@@ -1,29 +1,43 @@
-const modalAmin = new bootstrap.Modal(
-    document.getElementById("contactoAdmin")
-  );
-  const abrirModalAdmin = document.getElementById("abrirModalAdmin");
-  
-  const mostrarModalAdmin = () => {
-    modalAmin.show();
-  };
-  
-  abrirModalAdmin.addEventListener("click", mostrarModalAdmin);
+const modalAmin = new bootstrap.Modal(document.getElementById("contactoAdmin"));
+const abrirModalAdmin = document.getElementById("abrirModalAdmin");
 
 
-  function loguear()
-  {
-      let usuario=document.getElementById("usuario").value;
-      let contraseña=document.getElementById("clave").value;
-      let errorMensaje = document.getElementById("errorMensaje");
+
+const mostrarModalAdmin = () => {
+  modalAmin.show();
   
-      if (usuario.trim() === "" || contraseña.trim() === "") {
-        errorMensaje.textContent = "Por favor, ingrese usuario y contraseña.";
-        return;
-    }
+};
+
+abrirModalAdmin.addEventListener("click", mostrarModalAdmin);
+
+
+function iniciarSesion() {
+  let usuario = document.getElementById("usuario").value;
+  let contraseña = document.getElementById("clave").value;
+  let errorMensaje = document.getElementById("errorMensaje");
+
+
+  if (usuario.trim() === "" || contraseña.trim() === "") {
+    errorMensaje.textContent = "Por favor, ingrese usuario y contraseña.";
+    return;
+  }
+
+  if (usuario === "Gamer" && contraseña === "1234") {
+    window.location = "administracion.html";
+  } else errorMensaje.textContent = "Usuario o Contraseña incorrectos";
+
+  limpiarFormulario();
+}
+
+function limpiarFormulario() {
+    formLogin.reset();
     
-  if(usuario==="Gamer" && contraseña==="1234"){
-      window.location="administracion.html"
-  }
-  else
-  errorMensaje.textContent = "Usuario o Contraseña incorrectos";
-  }
+}
+
+function cerrar() {
+    document.getElementById("usuario").value = "";
+    document.getElementById("clave").value = "";
+    document.getElementById("errorMensaje").textContent = "";
+}
+
+
