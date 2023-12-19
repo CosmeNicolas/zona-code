93b77f610986d2c77a70f9b71ed995d3442d7341
Pablo Moyano <pabmoy1725@gmail.com>
2023-12-19 00:47:38 -0300

    Se aplican mensajes de error y de ingreso a la pagina de Administrador

--- js/loginAdmin.js
+++ js/loginAdmin.js
@@ -2,6 +2,7 @@ function iniciarSesion() {
   let usuario = document.getElementById("usuario").value;
   let contraseña = document.getElementById("clave").value;
   let errorMensaje = document.getElementById("errorMensaje");
+  let mensajeDeIngreso = document.getElementById("mensajeDeIngreso");
 
   if (usuario.trim() === "" || contraseña.trim() === "") {
     errorMensaje.textContent = "Por favor, ingrese usuario y contraseña.";
@@ -13,35 +14,61 @@ function iniciarSesion() {
   }
 
   if (usuario === "Gamer" && contraseña === "1234") {
-    window.location = "administracion.html";
+    borrarmensaje();
+     mensajeDeIngreso.textContent =
+      "Datos ingresados correctamente!, se dirige a pagina administrador";
+    setTimeout(() => {
+      window.location = "administracion.html";
+    }, 6000);
   } else {
     errorMensaje.textContent = "Usuario o Contraseña incorrectos";
     limpiarFormulario();
   }
 }
+/* function verificarSesionIniciada(){
+  if (datosEnLocalStorage.length > 0) {
+    let contenedorLogin = document.getElementById("contenedorLogin")
+    contenedorLogin.innerHTML = `<h1>La sesión ya se encuentra iniciada. Vamos a redirigirte al panel de administrador...</h1>`
+    setTimeout(() => {
+      window.location.href = window.origin + "/pages/administrador.html";
+    }, 5000);
+  }
+} */
 
 function validarCampos(usuario, contraseña) {
   const soloLetrasNumeros = /^[a-zA-Z0-9]+$/;
   const soloNumeros = /^\d+$/;
 
-  if (usuario.trim() === "" || !soloLetrasNumeros.test(usuario) || usuario.length < 4 || usuario.length > 15) {
-    errorMensaje.textContent =
-      "El usuario debe contener letras y/o números.";
-      limpiarFormulario();
+  if (
+    usuario.trim() === "" ||
+    !soloLetrasNumeros.test(usuario) ||
+    usuario.length < 4 ||
+    usuario.length > 15
+  ) {
+    errorMensaje.textContent = "El usuario debe contener letras y/o números.";
+    limpiarFormulario();
     return false;
   }
 
-  if (contraseña.trim() === "" || !soloNumeros.test(contraseña) || contraseña.length < 4 || contraseña.length > 15) {
+  if (
+    contraseña.trim() === "" ||
+    !soloNumeros.test(contraseña) ||
+    contraseña.length < 4 ||
+    contraseña.length > 15
+  ) {
     errorMensaje.textContent = "La contraseña debe contener solo números.";
     limpiarFormulario();
     return false;
   }
 
   return true;
-  
 }
 
 function limpiarFormulario() {
   formLogin.reset();
+  
 }
 
+function borrarmensaje() {
+  document.getElementById("errorMensaje").textContent = "";
+}
